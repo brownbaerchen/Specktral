b938b5fc792f4eec2d98c02f1789729009ea6bb6
Thomas Baumann <39156931+brownbaerchen@users.noreply.github.com>
2026-06-08 16:16:13 +0200

    Added composite differentiation matrices

--- specktral/composite.py
+++ specktral/composite.py
@@ -1,14 +1,18 @@
 import numpy as np
+import scipy
+import logging
 
 
 class CompositeBase:
     xp = np
+    sparse_lib = scipy.sparse
 
     def __init__(self, bases):
         assert isinstance(bases, (list, tuple)), (
             f"You have to supply the bases for a composite base as tuple or list but gave {type(bases)}"
         )
         self.bases = bases
+        self.logger = logging.getLogger(name="Spectral Discretization")
 
     @property
     def ndim(self):
@@ -27,3 +31,92 @@ class CompositeBase:
         """
         grids = [self.bases[i].get_wavenumbers() for i in range(self.ndim)]
         return self.xp.meshgrid(*grids, indexing="ij")
+
+    def transform(self, u, axes=None):
+        axes = axes if axes else tuple(i for i in range(self.ndim))
+
+        u_hat = u.copy()
+        for axis in axes:
+            u_hat = self.bases[axis].transform(u_hat, axes=(axis,))
+
+        return u_hat
+
+    def itransform(self, u_hat, axes=None):
+        axes = axes if axes else tuple(i for i in range(self.ndim))
+
+        u = u_hat.copy()
+        for axis in axes:
+            u = self.bases[axis].itransform(u, axes=(axis,))
+
+        return u
+
+    def expand_matrix_ND(self, matrix, aligned):
+        sp = self.sparse_lib
+        axes = np.delete(np.arange(self.ndim), aligned)
+        ndim = len(axes) + 1
+
+        if ndim == 1:
+            mat = matrix
+        elif ndim == 2:
+            axis = axes[0]
+            I1D = sp.eye(self.bases[axis].N)
+
+            mats = [None] * ndim
+            mats[aligned] = matrix
+            mats[axis] = I1D
+
+            mat = sp.kron(*mats)
+        elif ndim == 3:
+            mats = [None] * ndim
+            mats[aligned] = matrix
+            for axis in axes:
+                I1D = sp.eye(self.bases[axis].N)
+                mats[axis] = I1D
+
+            mat = sp.kron(mats[0], sp.kron(*mats[1:]))
+
+        else:
+            raise NotImplementedError(
+                f"Matrix expansion not implemented for {ndim} dimensions!"
+            )
+
+        mat = self.eliminate_zeros(mat)
+        return mat
+
+    def get_differentiation_matrix(self, axes, **kwargs):
+        """
+        Get differentiation matrix along specified axis. `kwargs` are forwarded to the 1D base implementation.
+
+        Args:
+            axes (tuple): Axes along which to differentiate.
+
+        Returns:
+            sparse differentiation matrix
+        """
+        D = self.expand_matrix_ND(
+            self.bases[axes[0]].get_differentiation_matrix(**kwargs), axes[0]
+        )
+        for axis in axes[1:]:
+            _D = self.bases[axis].get_differentiation_matrix(**kwargs)
+            D = D @ self.expand_matrix_ND(_D, axis)
+
+        self.logger.debug(
+            f"Set up differentiation matrix along axes {axes} with kwargs {kwargs}"
+        )
+        return D
+
+    def eliminate_zeros(self, A):
+        """
+        Eliminate zeros from sparse matrix. This can reduce memory footprint of matrices somewhat.
+        Note: At the time of writing, there are memory problems in the cupy implementation of `eliminate_zeros`.
+        Therefore, this function copies the matrix to host, eliminates the zeros there and then copies back to GPU.
+
+        Args:
+            A: sparse matrix to be pruned
+
+        Returns:
+            CSC sparse matrix
+        """
+        A = A.tocsc()
+        A.eliminate_zeros()
+        return A
--- tests/test_composite.py
+++ tests/test_composite.py
@@ -81,3 +81,47 @@ def test_composite_wave_number_grid():
     for i in range(x_base.N):
         for j in range(y_base.N):
             assert np.allclose(grid[2][i, j, :], z_base.get_wavenumbers())
+
+
+@pytest.mark.parametrize("dim", [1, 2, 3])
+def test_differentiation_matrix(dim):
+    from specktral.composite import CompositeBase
+    from specktral.bases import Fourier, Chebychev
+
+    x_base = Fourier(N=128)
+    y_base = Chebychev(N=64)
+    z_base = Fourier(N=17)
+
+    bases = [x_base, y_base, z_base][:dim]
+
+    composite = CompositeBase(bases)
+    grid = composite.get_grid()
+
+    Dx = composite.get_differentiation_matrix(axes=(0,))
+    u = np.sin(2 * grid[0])
+    du_expect = 2 * np.cos(2 * grid[0])
+
+    u_hat = composite.transform(u)
+    du_hat = (Dx @ u_hat.flatten()).reshape(u_hat.shape)
+    du = composite.itransform(du_hat)
+    assert np.allclose(du, du_expect)
+
+    if dim > 1:
+        Dy = composite.get_differentiation_matrix(axes=(1,))
+        u = 4 * grid[1] ** 2 + 3
+        du_expect = 8 * grid[1]
+
+        u_hat = composite.transform(u)
+        du_hat = (Dy @ u_hat.flatten()).reshape(u_hat.shape)
+        du = composite.itransform(du_hat)
+        assert np.allclose(du, du_expect)
+
+    if dim == 3:
+        Dxz = composite.get_differentiation_matrix(axes=(0, 2))
+        u = np.sin(2 * grid[0]) * np.cos(4 * grid[2])
+        du_expect = 2 * np.cos(2 * grid[0]) * (-4) * np.sin(4 * grid[2])
+
+        u_hat = composite.transform(u)
+        du_hat = (Dxz @ u_hat.flatten()).reshape(u_hat.shape)
+        du = composite.itransform(du_hat)
+        assert np.allclose(du, du_expect)
